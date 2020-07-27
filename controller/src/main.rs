e670c6a3b96e1f42f08c242c569c61fee4a292d3
Praveen Perera <ppraveen25@gmail.com>
2020-07-27 10:47:30 -0400

    Refactor to use individual watchers

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,96 +1,15 @@
+use certmaster::crd::{Cert, CertIssuer};
+use certmaster::store::Store;
 use futures::prelude::*;
-use k8s_openapi::api::core::v1::Secret;
-use k8s_openapi::apimachinery::pkg::apis::meta::v1::{ObjectMeta, OwnerReference};
-use k8s_openapi::ByteString;
 use kube::{
-    api::{Api, ListParams, Meta, PostParams},
+    api::{Api, ListParams, Meta},
     Client,
 };
-use kube_derive::CustomResource;
-use kube_runtime::controller::{Context, Controller, ReconcilerAction};
 use kube_runtime::watcher;
 use log::{info, warn};
-use rweb::Schema;
-use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tokio;
 use tokio::task;
-use tokio::{sync::RwLock, time::Duration};
-
-#[derive(CustomResource, Serialize, Deserialize, Clone, Debug, Schema)]
-#[kube(group = "certmaster.kuberails.com", version = "v1")]
-#[serde(rename_all = "camelCase")]
-pub struct CertIssuerSpec {
-    domain_name: String,
-    dns_provider: DnsProviderSpec,
-    secret_name: Option<String>,
-    #[serde(default = "default_namespace")]
-    namespaces: Vec<String>,
-}
-
-fn default_namespace() -> Vec<String> {
-    vec!["default".to_string()]
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, Schema)]
-#[serde(rename_all = "camelCase")]
-struct DnsProviderSpec {
-    provider: DnsProvider,
-    key: String,
-    secret_key: String,
-}
-
-#[derive(Serialize, Deserialize, Clone, Debug, Schema)]
-enum DnsProvider {
-    #[serde(rename = "digtalocean")]
-    DigitalOcean,
-    #[serde(rename = "cloudflare")]
-    Cloudflare,
-}
-
-#[derive(Debug, Error)]
-enum Error {
-    #[error(transparent)]
-    KubeError(#[from] kube::error::Error),
-    #[error("missing object key in {0})")]
-    MissingObjectKey(&'static str),
-}
-
-type State = Context<RwLock<InnerState>>;
-
-struct InnerState {
-    client: Client,
-    cert_issuers: Vec<CertIssuer>,
-    certs: Vec<Secret>,
-}
-
-impl InnerState {
-    fn new(client: Client) -> InnerState {
-        InnerState {
-            client: client,
-            cert_issuers: vec![],
-            certs: vec![],
-        }
-    }
-}
-
-async fn add_cert_issuer(state: &State, cert_issuer: &CertIssuer) -> () {
-    state
-        .get_ref()
-        .write()
-        .await
-        .cert_issuers
-        .push(cert_issuer.clone());
-}
-
-async fn delete_cert_issuer(state: &State, cert_issuer: &CertIssuer) -> () {
-    state
-        .get_ref()
-        .write()
-        .await
-        .cert_issuers
-        .retain(|ci| ci.meta().name != cert_issuer.meta().name);
-}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -98,108 +17,50 @@ async fn main() -> anyhow::Result<()> {
 
     let client = Client::try_default().await?;
 
-    // for controller
     let cert_issuer: Api<CertIssuer> = Api::all(client.clone());
-    let context = Context::new(RwLock::new(InnerState::new(client.clone())));
+    let store = Store::new(client.clone());
+    let certs: Api<Cert> = Api::all(client.clone());
 
-    // for watcher
-    let cert_issuer_clone = cert_issuer.clone();
-    let context_clone = context.clone();
-
-    let certs: Api<Secret> = Api::all(client.clone());
-
-    let controller_task = task::spawn(async move {
-        Controller::new(cert_issuer, ListParams::default().include_uninitialized())
-            .owns(certs.clone(), ListParams::default())
-            .run(reconcile, error_policy, context)
-            .for_each(|res| async move {
-                match res {
-                    Ok(o) => info!("reconciled {:?}", o),
-                    Err(e) => warn!("reconcile failed: {}", e),
-                }
-            })
-            .await;
-    });
-
-    let watcher_task = task::spawn(async move {
-        let watcher = watcher(cert_issuer_clone, ListParams::default());
-        let _ = watcher
-            .try_for_each(|event| async {
-                match event {
-                    watcher::Event::Deleted(cert_issuer) => {
-                        handle_delete(cert_issuer, context_clone.clone()).await;
-                    }
-                    _event => (),
-                }
-                Ok(())
-            })
-            .await;
-    });
-
-    let _ = futures::join!(controller_task, watcher_task);
+    tokio::join!(
+        cert_issuer_watcher(cert_issuer, store.clone()),
+        cert_watcher(certs, store)
+    );
 
     Ok(())
 }
 
-async fn reconcile(cert_issuer: CertIssuer, state: State) -> Result<ReconcilerAction, Error> {
-    info!("Cert Issuer Reconciled: {:#?}", cert_issuer);
+async fn cert_issuer_watcher(api: Api<CertIssuer>, store: Store) {
+    task::spawn(async move {
+        let watcher = watcher(api, ListParams::default());
+        let _ = watcher
+            .try_for_each(|event| handle_cert_issuer_events(event, store.clone()))
+            .await;
 
-    add_cert_issuer(&state, &cert_issuer).await;
-
-    let mut contents = std::collections::BTreeMap::new();
-    contents.insert(
-        "content".to_string(),
-        ByteString("hello".as_bytes().to_vec()),
-    );
-
-    let cert = Secret {
-        metadata: ObjectMeta {
-            name: cert_issuer.metadata.name.clone(),
-            namespace: Some("avencera".to_string()),
-            owner_references: Some(vec![OwnerReference {
-                controller: Some(true),
-                ..object_to_owner_reference::<CertIssuer>(cert_issuer.metadata.clone())?
-            }]),
-            ..ObjectMeta::default()
-        },
-        data: Some(contents),
-        ..Default::default()
-    };
-
-    let cert_api =
-        Api::<Secret>::namespaced(state.get_ref().read().await.client.clone(), "avencera");
-    let pp = PostParams::default();
-
-    let a = cert_api.create(&pp, &cert).await;
-
-    println!("DONE: {:#?}", a);
-
-    Ok(ReconcilerAction {
-        requeue_after: Some(Duration::from_secs(60 * 10)),
-    })
+        ()
+    });
 }
 
-async fn handle_delete(cert_issuer: CertIssuer, state: State) {
-    info!("Cert Issuer deleted: {:?}", cert_issuer);
-    delete_cert_issuer(&state, &cert_issuer).await;
+async fn cert_watcher(api: Api<Cert>, store: Store) {
+    task::spawn(async move {
+        let watcher = watcher(api, ListParams::default());
+        let _ = watcher
+            .try_for_each(|event| handle_cert_events(event, store.clone()))
+            .await;
+
+        ()
+    });
 }
 
-fn error_policy(error: &Error, _state: State) -> ReconcilerAction {
-    warn!("Error policy triggered: {:#?}", error);
-
-    ReconcilerAction {
-        requeue_after: Some(Duration::from_secs(1)),
-    }
+async fn handle_cert_issuer_events(
+    event: watcher::Event<CertIssuer>,
+    store: Store,
+) -> Result<(), watcher::Error> {
+    Ok(())
 }
 
-fn object_to_owner_reference<K: Meta>(meta: ObjectMeta) -> Result<OwnerReference, Error> {
-    Ok(OwnerReference {
-        api_version: K::API_VERSION.to_string(),
-        kind: K::KIND.to_string(),
-        name: meta.name.ok_or(Error::MissingObjectKey(".metadata.name"))?,
-        uid: meta
-            .uid
-            .ok_or(Error::MissingObjectKey(".metadata.backtrace"))?,
-        ..OwnerReference::default()
-    })
+async fn handle_cert_events(
+    event: watcher::Event<Cert>,
+    store: Store,
+) -> Result<(), watcher::Error> {
+    Ok(())
 }
